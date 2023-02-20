0e896e377e08b6cf80dc476044b8ad76a34042b5
IanMarcoMoffett <svr.mkall@gmail.com>
2023-02-20 18:08:27 -0500

    Feat: Boot when key is pressed

---
+++ src/include/def.h
@@ -0,0 +1,10 @@
+#ifndef _DEF_H_
+#define _DEF_H_
+
+#include <efi.h>
+
+
+extern EFI_BOOT_SERVICES* BS;
+extern EFI_SYSTEM_TABLE* ST;
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,14 +1,23 @@
 #include <efi.h>
 #include <printf.h>
 
+EFI_BOOT_SERVICES* BS;
+EFI_SYSTEM_TABLE* ST;
+
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
 {
   (void)ImageHandle;
   printf_init(SystemTable);
-
   clear_screen();
-  printf("-- Welcome to ZebraLoader --\n");
-  printf("Hello, wold!\n");
+
+  ST = SystemTable;
+  BS = ST->BootServices;
+  
+  printf("Press any key to boot\n");
+  while (uefi_call_wrapper(BS->CheckEvent, 1, ST->ConIn->WaitForKey))
+  {
+    uefi_call_wrapper(BS->Stall, 1, 1000);
+  }
 
   for (;;);
 }
