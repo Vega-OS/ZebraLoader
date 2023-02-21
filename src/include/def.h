00ecadf1b18eaf29d740704d4e02cbd1afac7f57
IanMarcoMoffett <svr.mkall@gmail.com>
2023-02-20 20:50:10 -0500

    Feat: Add halt() macro

--- src/include/def.h
+++ src/include/def.h
@@ -8,8 +8,11 @@
 
 #include <efi.h>
 
-
 extern EFI_BOOT_SERVICES* BS;
 extern EFI_SYSTEM_TABLE* ST;
+
+#define halt()                        \
+  printf("-- System halted --\n");    \
+  __asm__ __volatile__("cli; hlt")
 
 #endif
