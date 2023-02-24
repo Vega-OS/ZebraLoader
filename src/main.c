c094bc6bf94e566c4eab717f6e05903ace1e2c23
queso-fuego <fuegoqueso@gmail.com>
2023-02-23 23:36:29 -0600

    Add gop_init() function, and temp change to take keystroke for escape key to exit bootloader.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <def.h>
+#include <dev/gop.h>
 
 EFI_STATUS efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* st)
 {
@@ -11,6 +12,20 @@ EFI_STATUS efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* st)
                     0,
                     NULL
   );
-  
+
+  // Verify GOP and set native mode
+  gop_init();
+
+  // Press escape to exit from bootloader
+  while (1) {
+    EFI_INPUT_KEY key;
+    uefi_call_wrapper(ST->ConIn->ReadKeyStroke, 2, ST->ConIn, &key);
+
+    if ((UINTN)key.ScanCode == SCAN_ESC)
+    {
+      break;
+    }
+  }
+
   return EFI_SUCCESS;
 }
