8019e4785643d6d6ef9c5ab43425c4283c7a80ee
IanMarcoMoffett <svr.mkall@gmail.com>
2023-02-25 20:20:53 -0500

    loader: Check if ELF header is valid

--- src/loader.c
+++ src/loader.c
@@ -7,6 +7,19 @@
 #include <elf.h>
 #include <loader.h>
 
+/*
+ *  Returns 1 if the ELF header
+ *  passed is valid.
+ */
+
+static UINT8 is_eh_valid(Elf64_Ehdr *eh)
+{
+  return eh->e_ident[EI_CLASS] == ELFCLASS64
+         && eh->e_ident[EI_DATA] == ELFDATA2LSB
+         && eh->e_type == ET_EXEC
+         && eh->e_machine == EM_X86_64;
+}
+
 void load_kernel(CHAR16 *file_name)
 {
   // Get file
@@ -55,6 +68,12 @@ void load_kernel(CHAR16 *file_name)
 
   // Parse elf header
   Elf64_Ehdr *elf_hdr = (Elf64_Ehdr *)file_buffer;
+
+  if (!is_eh_valid(elf_hdr))
+  {
+    Print(L"Kernel ELF header not valid!\n");
+    return;
+  }
 
   Print(L"Found ELF entry point: 0x%x\n", elf_hdr->e_entry);
 }
